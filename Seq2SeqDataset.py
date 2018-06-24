e469b10b91cd44b6754460f4c8ac65e586ada0b7
kosuke futamata <f-e@toki.waseda.jp>
2018-06-25 02:08:38 +0900

    update preprocess

--- Seq2SeqDataset.py
+++ Seq2SeqDataset.py
@@ -5,6 +5,7 @@ import pickle
 from pathlib import Path
 import chainer
 
+
 class Seq2SeqDatasetBase(chainer.dataset.DatasetMixin):
     def __init__(
             self,
@@ -15,24 +16,30 @@ class Seq2SeqDatasetBase(chainer.dataset.DatasetMixin):
             n_target_min_tokens,
             n_target_max_tokens,
             validation=False
-        ):
+            ):
 
-        if Path(source_sentence_path).exists() and Path(target_sentence_path).exists():
+        if Path(source_sentence_path).exists() \
+                and Path(target_sentence_path).exists():
             source_data = self.load_data(source_sentence_path)
             target_data = self.load_data(target_sentence_path)
             assert len(source_data['train']) == len(target_data['train'])
 
         self.pairs = [
-            (np.array(s['encoded_tokens'], np.int32), np.array(t['encoded_tokens'], np.int32)) 
+            (np.array(s['encoded_tokens'], np.int32),
+                np.array(t['encoded_tokens'], np.int32))
             for s, t in zip(source_data['train'], target_data['train'])
             if n_source_min_tokens <= len(s['encoded_tokens']) <= n_source_max_tokens and
-               n_target_min_tokens <= len(t['encoded_tokens']) <= n_target_max_tokens
+            n_target_min_tokens <= len(t['encoded_tokens']) <= n_target_max_tokens
             ]
 
         self.source_word_ids = source_data['word_ids']
         self.target_word_ids = target_data['word_ids']
-        self.inv_source_word_ids = { v: k for k, v in source_data['word_ids'].items() }
-        self.inv_target_word_ids = { v: k for k, v in target_data['word_ids'].items() }
+        self.inv_source_word_ids = {
+                v: k for k, v in source_data['word_ids'].items()
+                }
+        self.inv_target_word_ids = {
+                v: k for k, v in target_data['word_ids'].items()
+                }
 
         if validation:
             pass
@@ -49,7 +56,8 @@ class Seq2SeqDatasetBase(chainer.dataset.DatasetMixin):
             with open(path, 'r') as f:
                 data = json.load(f)
         else:
-            msg = 'File %s can be loaded.\n choose json or pickle format' % path
+            msg = 'File %s can be loaded.\n \
+                    choose json or pickle format' % path
             raise TypeError(msg)
 
         return data
@@ -57,15 +65,14 @@ class Seq2SeqDatasetBase(chainer.dataset.DatasetMixin):
     def calc_unk_ratio(self, data):
         unk = sum((s == self.source_word_ids['<UNK>']).sum() for s in data)
         words = sum(s.size for s in data)
-        
+
         return (unk / words) * 100
 
-
     def index2token(self, indices, inv_word_ids):
-        return [ inv_word_ids[index] for index in indices]
+        return [inv_word_ids[index] for index in indices]
 
     def token2index(self, tokens, word_ids):
-        return [ word_ids[token] for token in tokens ]
+        return [word_ids[token] for token in tokens]
 
     def get_example(self, i):
         return self.pairs[i]
@@ -101,5 +108,3 @@ class Seq2SeqDatasetBase(chainer.dataset.DatasetMixin):
     @property
     def target_index2token(self, indices):
         return self.index2token(indices, self.inv_target_word_ids)
-
-
